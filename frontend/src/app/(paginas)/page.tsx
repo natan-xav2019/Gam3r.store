11cf13e489c551133ca44ac4d803bb0c97e59dcf
Natan Xavier <natanxaviersiquera@gmail.com>
2024-10-02 06:05:35 -0300

    Feat(template): Pagina

---
+++ frontend/src/app/(paginas)/page.tsx
@@ -0,0 +1,10 @@
+import ListaProdutos from "@/components/produto/ListaProdutos";
+import { produtos } from "@/core";
+
+export default function Home() {
+  return (
+    <div className="flex-1 flex flex-col container gap-5 py-10">
+      <ListaProdutos />
+    </div>
+  )
+}
